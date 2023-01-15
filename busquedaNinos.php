5541c0b1c8ccd4eaff7dcc04da6abf375a92d5b9
Carballo Alamo Francisco Manuel <97671162+franciscoBanderas@users.noreply.github.com>
2023-01-16 00:42:12 +0100

    Add files via upload

---
+++ busquedaNinos.php
@@ -0,0 +1,42 @@
+<?php 
+    require_once('./modelos/Ninos.php');
+    $modelNinos = new Ninos();
+    $filas = $modelNinos->seleccionarTodos();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <title>Busqueda </title>
+</head>
+<body>
+
+    <div class="contenedor">
+        <div class="fila">
+            <div class="col-12 col-md-8 offset-md-2 mt-4">
+                <h1>Búsqueda</h1>      
+            </div>
+            <?php if((int)$filas->num_rows){ ?>
+                <div class="col-12 col-md-8 offset-md-2 mt-4">
+                    <form action="pedidosNinos.php" method="post">
+                        <div class="form-group">
+                            <label for="id" class="mb-2">Seleccione un niño</label>
+                            <select class="form-select" aria-label="Default select example" name="id" required>
+                                <?php while($fila = $filas->fetch_assoc()){ ?>
+                                <option value="<?php echo $fila['id']; ?>"><?php echo $fila['nombreNino']; ?> <?php echo $fila['apellidosNino']; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div><br>
+                        <button type="submit" class="btn btn-outline-info">Ver su lista de regalos</button>
+                    </form>
+                </div>
+            <?php } ?> 
+            
+    </div>
+
+</body>
+</html>
